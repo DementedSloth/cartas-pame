52c00a8a3ffcf3e132907c1014ef264748a83398
DementedSloth <96711445+DementedSloth@users.noreply.github.com>
2026-08-24 18:01:40 -0300

    Update CACHE_NAME to version v25

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 // a abrir la app). Entre las tres cosas, una actualización nueva debería
 // notarse en, como mucho, un minuto.
 
-const CACHE_NAME = 'framecorreo-v24'; // subir esta versión en cada deploy: v3, v4, v5, v6, v7, v8, v9, v10, v11...
+const CACHE_NAME = 'framecorreo-v25'; // subir esta versión en cada deploy: v3, v4, v5, v6, v7, v8, v9, v10, v11...
 const APP_SHELL = [
   './',
   './index.html',
